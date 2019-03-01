f3c4d7e0512e8340d8286af839027b96d7f52f62
donis <danis.danilyuk@gmail.com>
2019-03-01 21:04:39 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        /*
+             8206
+            /2 = 0
+            /3 = 1
+            /5 = 1
+            /7 = 2
+            /11 = 0
+        */
+
+    }
+}
